863d7b3eae1b8d325378b25a51b45fd2c548263f
Ty Laughlin <tycamlau@yahoo.com>
2022-09-27 15:59:48 +0000

    This is the script for Lab 4

---
+++ Lab/Outputs/LaughlinLab4_UserInput.R
@@ -0,0 +1,30 @@
+
+
+file_name <- readline()
+MachineRead_output <- read.csv(file_name)
+
+MachineRead_output$`Paper.ID` <- gsub("\\[", "", MachineRead_output$`Paper.ID`)
+MachineRead_output$`Paper.ID` <- gsub("\\'", "", MachineRead_output$`Paper.ID`)
+MachineRead_output$`Paper.ID` <- gsub("\\]", "", MachineRead_output$`Paper.ID`)
+MachineRead_output$`Paper.ID` <- gsub(" ", "", MachineRead_output$`Paper.ID`)
+
+
+paperID <- c()
+
+
+for (each in MachineRead_output$Paper.ID) {
+  
+  paperID <- unlist(strsplit(each, ","))
+  
+  rowNumbers <- which(each == MachineRead_output$Paper.ID)
+  
+  MachineRead_output$uniqueNumID[rowNumbers] <- length(unique(paperID))
+  
+}
+
+
+rowsToRemove <- which(MachineRead_output$uniqueNumID == 1)
+
+MachineRead_output2 <- MachineRead_output[-c(rowsToRemove),]
+
+
